7019ef4115fab1ff97f225c69dabdb35470d0b0f
INvSKnight <114570261+INvSKnight@users.noreply.github.com>
2024-01-27 18:00:18 +0100

    xbfv

---
+++ Assets/voiceinput.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class voiceinput : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    private void Start()
+    {
+        actions.Add("left", Up);
+        actions.Add("right", Down);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Up()
+    {
+        transform.Translate(0, 1, 0);
+    }
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+}
