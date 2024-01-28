10cdd8dba4e2e698c8aa557428c447b2af794939
Steinar <steinar.vp@gmail.com>
2024-01-28 13:24:19 +0100

    King has arrived

--- Assets/FunCounter.cs
+++ Assets/FunCounter.cs
@@ -1,14 +1,26 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FunCounter : MonoBehaviour
 {
+    private TextMeshProUGUI label;
+    private int funPoints = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddFunPoints(0);
     }
+
+    void AddFunPoints(int points)
+    {
+        funPoints += points;
+        label.text = "Funs: " + funPoints;
+    }
+
 
     // Update is called once per frame
     void Update()
